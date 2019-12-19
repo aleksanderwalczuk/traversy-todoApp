b7f6932140452dffb89839f67fae9f9789f24c93
rednaxela700 <ohleg@windowslive.com>
2019-12-19 21:53:13 +0100

    rendering todos title via props

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,9 +3,9 @@ import React from 'react';
 export class Todos extends React.Component {
   render () {
     return (
-      <div className="App">
-        <h1>Todos</h1>
-      </div>
+     this.props.todos.map(todo => (
+     <h3>{todo.title}</h3>
+     ))
     );
   }
 }

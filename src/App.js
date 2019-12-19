4591503b048ca30cf7f3dae8db2bec7c2db41422
rednaxela700 <ohleg@windowslive.com>
2019-12-19 21:49:14 +0100

    added todos to state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.sass';
 import Todos from './components/Todos';
 
 class App extends React.Component {
+  
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'make dinner',
+        complete: false
+      },
+      {
+        id: 2,
+        title: 'mop the floor',
+        complete: false
+      },
+      {
+        id: 3,
+        title: 'clean the bathroom',
+        complete: false
+      }
+    ]
+  }
   render () {
     return (
       <div className="App">
